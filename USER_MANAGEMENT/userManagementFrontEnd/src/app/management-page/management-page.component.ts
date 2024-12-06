78f4934b628aecfaee2e8835a97a500419cf0177
DevBeatrizAranha <132088012+DevBeatrizAranha@users.noreply.github.com>
2024-12-06 19:15:03 -0300

    adicionando header

--- USER_MANAGEMENT/userManagementFrontEnd/src/app/management-page/management-page.component.ts
+++ USER_MANAGEMENT/userManagementFrontEnd/src/app/management-page/management-page.component.ts
@@ -3,13 +3,14 @@ import { FormsModule } from '@angular/forms';
 
 import { FormComponent } from '../components/form/form.component';
 import { UserListComponent } from '../components/user-list/user-list.component';
+import { HeaderComponent } from '../components/header/header.component';
 import * as bootstrap from 'bootstrap';
 
 
 
 @Component({
   selector: 'app-management-page',
-  imports: [ FormComponent, UserListComponent, FormsModule],
+  imports: [ FormComponent, UserListComponent, FormsModule, HeaderComponent],
   templateUrl: './management-page.component.html',
   styleUrl: './management-page.component.css'
 })
